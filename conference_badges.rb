c5570248de2471974aa587324385786b0ccc766a
Kameron Hungerford <kam.hungerford@gmail.com>
2020-07-14 02:51:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ end
 def batch_badge_creator(attendees)
   new_array = []
   attendees.each do |badge|
-    new_array << "Hello, my name is #{badge}"
+    new_array << "Hello, my name is #{badge}."
   end
   return new_array
 end
