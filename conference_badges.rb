6b80d845b7a7fb94868514572b77028b7b981065
Kameron Hungerford <kam.hungerford@gmail.com>
2020-07-14 02:54:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,5 +13,8 @@ end
 def assign_rooms(badge)
   new_array = []
   counter = 1
-    
+  badge.each do |room|
+    new_array << "Hello, #{badge}! You'll be assigned to the room #{room}!"
+  end
+  return new_array
 end
